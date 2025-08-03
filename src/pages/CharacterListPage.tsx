856ea38a35efe420652b21a2b77ce5af26d5d69b
Keshav Dussal <ksdussal96@gmail.com>
2025-08-03 14:10:18 +0530

    pagination,table and refresh enhancements

--- src/pages/CharacterListPage.tsx
+++ src/pages/CharacterListPage.tsx
@@ -1,7 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import { fetchCharacters } from '@/api/rickAndMorty'
 import { useSearch, useNavigate } from '@tanstack/react-router'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  createColumnHelper,
+} from '@tanstack/react-table'
 import type {
   CharacterBase,
   CharactersResponse,
@@ -11,7 +17,7 @@ export default function CharacterListPage() {
   const { page = 1 } = useSearch({ strict: false }) as { page?: number }
   const navigate = useNavigate()
   const [isRefreshing, setIsRefreshing] = useState(false)
-
+  const LAST_PAGE = 42
   const { data, isLoading, isError, refetch } = useQuery<CharactersResponse>({
     queryKey: ['characters', page],
     queryFn: () => fetchCharacters(page),
@@ -34,14 +40,71 @@ export default function CharacterListPage() {
     setIsRefreshing(false)
   }
 
+  // Define TanStack Table columns
+  const columnHelper = createColumnHelper<CharacterBase>()
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor('id', {
+        header: 'ID',
+        cell: (info) => info.getValue(),
+      }),
+      columnHelper.accessor('name', {
+        header: 'Name',
+        cell: (info) => info.getValue(),
+      }),
+      columnHelper.accessor('status', {
+        header: 'Status',
+        cell: (info) => {
+          const status = info.getValue()
+          let bgColor = ''
+
+          switch (status) {
+            case 'Alive':
+              bgColor = 'bg-green-600'
+              break
+            case 'Dead':
+              bgColor = 'bg-red-600'
+              break
+            case 'unknown':
+              bgColor = 'bg-gray-600'
+              break
+            default:
+              bgColor = 'bg-black'
+          }
+
+          return (
+            <span
+              className={`px-[0.2rem] py-[0.05rem] rounded text-white ${bgColor}`}
+            >
+              {status}
+            </span>
+          )
+        },
+      }),
+      columnHelper.accessor('species', {
+        header: 'Species',
+        cell: (info) => info.getValue(),
+      }),
+    ],
+    [],
+  )
+  // @ts-ignore
+  const table = useReactTable({
+    data: characters,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Character List</h1>
+      <h1 className="text-[1.5rem] font-bolder italic mb-4 underline mb-[1rem]">
+        Character List
+      </h1>
       <button
-        className="px-3 py-1 border rounded bg-blue-500 text-white"
+        className="px-[0.4rem] py-[0.2rem] border rounded bg-blue-500 text-white mb-[1rem]"
         onClick={handleRefresh}
       >
-        Refresh
+        Refresh 🔄
       </button>
 
       {(isLoading || isRefreshing) && <p>Refreshing...</p>}
@@ -50,44 +113,75 @@ export default function CharacterListPage() {
 
       {!isLoading && !isRefreshing && !isError && (
         <>
-          <table className="w-full border">
-            <thead>
-              <tr className="bg-gray-200">
-                <th className="p-2 border">ID</th>
-                <th className="p-2 border">Name</th>
-                <th className="p-2 border">Status</th>
-                <th className="p-2 border">Species</th>
-              </tr>
-            </thead>
-            <tbody>
-              {characters.map((char: CharacterBase) => (
-                <tr
-                  key={char.id}
-                  className="cursor-pointer hover:bg-gray-100"
-                  onClick={() => navigate({ to: `/character/${char.id}` })}
-                >
-                  <td className="p-2 border">{char.id}</td>
-                  <td className="p-2 border">{char.name}</td>
-                  <td className="p-2 border">{char.status}</td>
-                  <td className="p-2 border">{char.species}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className="overflow-x-auto mt-4 mx-[1rem]">
+            <table className="w-full border">
+              <thead className="bg-[#9F0712] text-white">
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <tr key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <th key={header.id} className="p-[0.3rem] border">
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody>
+                {table.getRowModel().rows.map((row) => (
+                  <tr
+                    key={row.id}
+                    className="cursor-pointer hover:bg-gray-100"
+                    onClick={() =>
+                      navigate({ to: `/character/${row.original.id}` })
+                    }
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <td key={cell.id} className="p-[0.1rem] border">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
-          <div className="mt-4 flex gap-4">
+          <div className="flex gap-4 items-center justify-center mt-[1rem] mb-[1rem]">
+            {/* Previous Button */}
             <button
               disabled={page === 1}
-              className="px-3 py-1 border rounded disabled:opacity-50"
+              className={`px-[0.4rem] py-[0.2rem] border rounded flex items-center gap-1 ${
+                page === 1 ? 'opacity-50 bg-[#D3D3D3] cursor-not-allowed' : ''
+              }`}
               onClick={() => changePage(page - 1)}
             >
+              {page === 1 && <span title="First page"></span>}
               Prev
             </button>
+
+            {/* Current Page Display */}
+            <span className="text-sm">Page {page}</span>
+
+            {/* Next Button */}
             <button
-              className="px-3 py-1 border rounded"
+              disabled={page === LAST_PAGE}
+              className={`px-[0.4rem] py-[0.2rem] border rounded flex items-center gap-1 ${
+                page === LAST_PAGE
+                  ? 'opacity-50 bg-[#D3D3D3] cursor-not-allowed'
+                  : ''
+              }`}
               onClick={() => changePage(page + 1)}
             >
               Next
+              {page === LAST_PAGE && <span title="Last page"></span>}
             </button>
           </div>
         </>
